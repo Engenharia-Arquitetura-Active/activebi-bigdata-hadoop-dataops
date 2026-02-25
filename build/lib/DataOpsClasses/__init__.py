76307f384e4add1d7063f9f0bace9c30c74682f8
leo.cobol@gmail.com <leo.cobol@gmail.com>
2026-02-25 14:14:07 -0300

    Refactor DataOpsClasses: replace HBaseRestClient with HBaseOps, update __all__ exports, and add SQLDataOps and Utils classes; increment version to 0.1.2

--- build/lib/DataOpsClasses/__init__.py
+++ build/lib/DataOpsClasses/__init__.py
@@ -1,5 +1,5 @@
-from .hbase_dataops import HBaseRestClient
+from .hbase_dataops import HBaseOps
 from .sql_dataops import SQLDataOps
 from .dataops_utils import Utils
 
-__all__ = ["HBaseRestClient", "SQLDataOps", "Utils"]
+__all__ = ["HBaseOps", "SQLDataOps", "Utils"]
