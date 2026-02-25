f0ccd51b29db82e13d8fb3f660747fec389b6e28
leo.cobol@gmail.com <leo.cobol@gmail.com>
2026-02-25 11:48:49 -0300

    Refactor project structure: rename package to DataOpsClasses, implement HBaseOps and SQLDataOps classes, and add utility methods in Utils class

--- src/HadoopDataOps/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hbase_dataops import HBaseRestClient
-from .dataops_utils import Utils
-
-__all__ = ["HBaseRestClient", "Utils"]
--- src/SQLDataOps/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sql_dataops import SQLDataOps
-from HadoopDataOps.dataops_utils import Utils
-
-__all__ = ["SQLDataOps", "Utils"]
